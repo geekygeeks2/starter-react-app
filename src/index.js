1bb402bf515c59e663a7b281e68214395c3fc4c5
Himanshupiet <hkc.kumar@gmail.com>
2024-02-18 21:30:06 +0530

    git second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// assets
+import 'assets/scss/style.scss';
+
+// third party
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+// project import
+import App from 'layout/App';
+import reducer from 'store/reducer';
+import * as serviceWorker from 'serviceWorker';
+
+const store = configureStore({ reducer });
+
+const root = createRoot(document.getElementById('root'));
+
+// ==============================|| MAIN - REACT DOM RENDER  ||==============
+
+root.render(
+  <Provider store={store}>
+    <BrowserRouter basename={process.env.REACT_APP_BASE_NAME}>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
